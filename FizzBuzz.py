8cf3876e2463bfd93b06bd693aed6415d06accce
HARSHVARDHAN PUROHIT <75427196+illumintor1609@users.noreply.github.com>
2021-07-09 22:40:24 +0530

    dev_commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,6 @@
-for number in range(0,101) :
+range_1 = int(input("Please enter start number : \n"))
+range_2 = int(input("Please enter end number : \n "))
+for number in range(range_1,range_2 + 1) :
   if number%3 == 0 and number%5 == 0 :
     print("FizzBuzz")
   elif number%3 == 0 :
